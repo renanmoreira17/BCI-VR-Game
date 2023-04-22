f66d3bf450b48915edf04f6b9b3be45ee75f937e
Renan Moreira <renanmoreira17@gmail.com>
2023-04-22 16:07:55 -0300

    added sky and clouds

---
+++ Assets/MyAssets/Scripts/MouseMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        // yaw = Mathf.Clamp(yaw, -90f, 90f);
+        // //the rotation range
+        // pitch = Mathf.Clamp(pitch, -60f, 90f);
+        // //the rotation range
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+    }
+}
